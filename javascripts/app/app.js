e8b2cd38cf095de85fbe985e0a54945a1f48565f
Nick Pelone <npelone@unca.edu>
2013-03-21 19:35:02 -0400

    Adds numbered delete buttons that are filled during the for loop setting up the JSON data

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -1,6 +1,6 @@
 (function () {
     "use strict";
-    var $ = window.$, toDoItemsLength;
+    var $ = window.$, toDoItemsLength, newItemsAdded;
     $(document).ready(console.log("Hello World"));
     //load json file for entries
     $.getJSON("data/all.json",  function(toDoData) {
@@ -9,7 +9,7 @@
         console.log(toDoItemsLength);
         //fill up tabs
         for (var i = 0; i < toDoItemsLength; i++){
-            $("#tabAllContent").append("<div class='toDoItem'><a>"+toDoData[i].description+"</a></div>");
-        }
-    });
+          $("#tabAllContent").append("<div class='toDoItem' id='"+i+"'> <a>"+toDoData[i].description+"</a><button class='delete' id='delete"+i+"'>Delete </button></div>");
+    }
+  });
 }());
