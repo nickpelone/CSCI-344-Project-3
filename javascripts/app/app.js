3901dc75a7a1a105e7eab18aca01f76f3072a424
Nick Pelone <npelone@unca.edu>
2013-03-21 19:50:56 -0400

    Fixes app.js to comply with JSLint (and it's absolutely silly rules for the for loop)

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -1,6 +1,6 @@
 (function () {
     "use strict";
-    var $ = window.$, toDoItemsLength, newItemsAdded;
+    var $ = window.$, toDoItemsLength, newItemsAdded, i;
     $(document).ready(console.log("Hello World"));
     //load json file for entries
     $.getJSON("data/all.json",  function(toDoData) {
@@ -8,8 +8,8 @@
         toDoItemsLength = toDoData.length;
         console.log(toDoItemsLength);
         //fill up tabs
-        for (var i = 0; i < toDoItemsLength; i++){
-          $("#tabAllContent").append("<div class='toDoItem' id='"+i+"'> <a>"+toDoData[i].description+"</a><button class='delete' id='delete"+i+"'>Delete </button></div>");
-    }
-  });
+        for (i = 0; i < toDoItemsLength; i += 1) {
+            $("#tabAllContent").append("<div class = 'toDoItem' id= '" + i + "'> <a>" + toDoData[i].description + "</a><button class = 'delete' id = 'delete" + i + "'>Delete </button></div>");
+        }
+    });
 }());
