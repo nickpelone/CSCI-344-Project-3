d22acbf869f0e93737eb7d417831f4bd2868ffb3
Nick Pelone <npelone@unca.edu>
2013-03-21 18:59:10 -0400

    Basic functionality: Loads items from JSON file and displays them in the 'all' tab

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -1,33 +1,15 @@
-var main = function () {
-  console.log("hello world!");
-  
-  var setUpClickHandler = function (anchor) {
-    anchor.click(function () {
-      var target = $(this).attr("href");
-    
-      $(".active").removeClass("active");
-      $(this).addClass("active");
-      $("#"+target).addClass("active");
-    
-      return false;
-    });    
-  };
-  
-  var setUpJSONTab = function (tab) {
-    var tab_a = $("<a>"+tab.title+"</a>").addClass("tab").attr("href", tab.title);
-    $(".tabs").append(tab_a);
-    
-    var content = $("<div>"+tab.content+"</div>").addClass("tab").attr("id",tab.title);
-    $(".content").append(content);
-    setUpClickHandler(tab_a);    
-  };
-
-  $.getJSON("tabs/tab1.json", setUpJSONTab);
-  $.getJSON("tabs/tab2.json", setUpJSONTab);  
-  $.getJSON("tabs/tab3.json", setUpJSONTab);  
-  
-  console.log("about to set up click handlers");
-  setUpClickHandler($(".tabs .tab"));
-}
-
-$(document).ready(main);
+(function () {
+    "use strict";
+    var $ = window.$, toDoItemsLength;
+    $(document).ready(console.log("Hello World"));
+    //load json file for entries
+    $.getJSON("data/all.json",  function(toDoData) {
+        console.log(toDoData[0].description);
+        toDoItemsLength = toDoData.length;
+        console.log(toDoItemsLength);
+        //fill up tabs
+        for (var i = 0; i < toDoItemsLength; i++){
+            $("#tabAllContent").append("<div class='toDoItem'><a>"+toDoData[i].description+"</a></div>");
+        }
+    });
+}());
