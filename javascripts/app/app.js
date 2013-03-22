f945af731f6374936a5070e408439fa36df70b8f
Nick Pelone <npelone@unca.edu>
2013-03-21 21:25:59 -0400

    Implements working delete button for a to-do item

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -1,15 +1,23 @@
 (function () {
     "use strict";
     var $ = window.$, toDoItemsLength, newItemsAdded, i;
-    $(document).ready(console.log("Hello World"));
-    //load json file for entries
+    
+   $(document).ready((function (){
+     //load json file for entries
     $.getJSON("data/all.json",  function(toDoData) {
         console.log(toDoData[0].description);
         toDoItemsLength = toDoData.length;
         console.log(toDoItemsLength);
         //fill up tabs
         for (i = 0; i < toDoItemsLength; i += 1) {
-            $("#tabAllContent").append("<div class = 'toDoItem' id= '" + i + "'> <a>" + toDoData[i].description + "</a><button class = 'delete' id = 'delete" + i + "'>Delete </button></div>");
+            $("#tabAllContent").append("<div class = 'toDoItem' id = '" + i + "'> <a>" + toDoData[i].description + "</a><button class = 'delete' id = '" + i + "'>Delete </button></div>");
+            
         }
+        $(".delete").click(function (){
+          var toDelete = $(this).attr("id");
+          console.log(toDelete);
+          $("#"+toDelete).fadeOut(1000);
+        });
     });
-}());
+  }));
+})();
